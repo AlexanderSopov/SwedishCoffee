588ca72a151cd3b32495dea5dadc7f41a30ddf8c
Alexander Sopov <sopovalexander@gmail.com>
2016-04-18 18:12:42 +0200

    started replacing stub elements with real content

--- website/views.py
+++ website/views.py
@@ -5,24 +5,8 @@ from .models import MenuItem, Projects
 
 def index(request):
 	menu = get_list_or_404(MenuItem, )
-	project = get_object_or_404(Projects, id=1)
-	print(project.imgUrl)
-	return render(request, "website/index.html", {
-		"page": {"title":"hej"},
-		"logoPath":"",
-		"menu": menu,
-		"projects":[
-			project,
-			{
-				"imgUrl": "website/images/background.png",
-				"introduction": "This is greatness, revisited",
-				"title": "Stub"
-			},
-			{
-				"imgUrl": "website/images/background.png",
-				"introduction": "This is greatness, revisited",
-				"title": "Stub"
-			},
+	project = get_list_or_404(Projects)
+	project1 = [
 			{
 				"imgUrl": "website/images/background.png",
 				"introduction": "This is greatness, revisited",
@@ -39,5 +23,11 @@ def index(request):
 				"title": "Stub"
 			}
 		]
-
+	projects= project+project1
+	print(projects)
+	return render(request, "website/index.html", {
+		"page": {"title":"hej"},
+		"logoPath":"",
+		"menu": menu,
+		"projects": projects
 	})	
